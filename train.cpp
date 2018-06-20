c942add8a93aa56c9947ec21d94265d89d748015
maeshu <simple.1994mae.shubest@gmail.com>
2018-06-21 01:57:57 +0900

    remake train.cpp

--- train.cpp
+++ train.cpp
@@ -1,63 +1,67 @@
 #include <iostream>
 
-
-int flag = 0;
-
 struct string_data
 {
-	char* buffer;
 	int length;
+	int buffer;
+	int esp_checker;
+	char* char_data;
+
+	string_data(int i, int j)
+	{
+		length = i;
+		buffer = j;
+		esp_checker = 1;
+		char_data = reinterpret_cast<char*>(malloc(sizeof(char) * buffer));
+	}
+
+	int input()
+	{
+		do
+		{
+			if (length > buffer)
+			{
+				char_data = (char*)realloc(char_data, sizeof(char) * (length + 32));
+				buffer += 32;
+			}
+			char_data[length++] = std::getchar();
+			if (char_data[length -1] == 27)
+			{
+				esp_checker = 0;
+				char_data[length -1] = '\0';
+				break;
+			}
+		} while (char_data[length-1] != '\n');
+	}
+
+	void output()
+	{
+		for (int i = 0; i < length; i++)
+		{
+			std::putchar(char_data[i]);
+		}
+	}
+
+	void free_buffer()
+	{
+		if (char_data)
+		{
+		free(char_data);
+		}
+		char_data = nullptr;
+	}
+
 };
 
-struct string_data input(struct string_data *data);
-void output(struct string_data *data);
-void free_buffer(struct string_data *data);
 
 int main()
 {
-	struct string_data moji;
-	buffer = dynamic_cast<char*>(malloc(sizeof(char)));
-	if (moji.buffer == nullptr)
+	struct string_data moji(0, 32);
+
+	while (moji.esp_checker)
 	{
-		return -1;
+		moji.input();
+		moji.output();
 	}
-		while (flag != 1)
-		{
-			output(moji.buffer, input());
-		}
-	free_buffer(moji.buffer);
-}
-
-
-struct string_data input(struct string_data *data)
-{
-	do
-	{
-		data->buffer[data->length] = std::getchar();
-		if (data->buffer[data->length] == 27)
-		{
-			flag = 1;
-			break;
-		}
-		data->buffer = (char*)realloc(data->buffer, sizeof(char) * (++data->length + 2));
-	} while (data->buffer[data->length] != '\0');
-	data->buffer[data->length] = '\0';
-	return data->length;
-}
-
-void output(struct string_data *data)
-{
-	for (int i = 0; i < data->length; i++)
-	{
-		std::putchar(data->buffer[i]);
-	}
-}
-
-void free_buffer(struct string_data *data)
-{
-	if (data->buffer)
-	{
-	free(data->buffer);
-	}
-	data-buffer = nullptr;
+	moji.free_buffer();
 }
